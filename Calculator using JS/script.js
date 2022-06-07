7a7aa567bf6cbd47d082a6e65b99ad5bc139df6c
sorima234 <103243758+sorima234@users.noreply.github.com>
2022-06-07 11:43:11 +0800

    Add files via upload

---
+++ Calculator using JS/script.js
@@ -0,0 +1,13 @@
+var result = document.getElementById(form1.answer.value);
+if (result == '+') {
+    console.log(" Sum is " + result)
+}  
+else if (result == '-') {
+    console.log(" Difference is " + result)
+}  
+else if (result == '*') { 
+    console.log(" Product is " + result)
+}  
+else {  
+    console.log(" Quotient is " + result)
+}  
